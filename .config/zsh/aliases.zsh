96682b566f5f60bd8dd5fe3d343917d9984b083f
kjcesar <kaitocesar@gmail.com>
2023-01-17 09:22:53 -0400

    configuration zsh update

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,5 @@
+alias pacup='sudo pacman -Syu' #easy update
+alias services='systemctl --type=service' #easy list services
+alias config='/usr/bin/git --git-dir=/home/kcesar/.cfg/ --work-tree=/home/kcesar' #github dotfiles sync
+alias at='alacritty-themes'
+alias cat='bat'
---
+++ .config/zsh/exports.zsh
@@ -0,0 +1,15 @@
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# Environment variables set everywhere
+export EDITOR="nvim"
+export TERMINAL="alacritty"
+export BROWSER="chromium"
+
+# For QT Themes
+# export QT_QPA_PLATFORMTHEME=qt5ct
+
+# XDG Paths
+export XDG_CONFIG_HOME=$HOME/.config
+export XDG_CACHE_HOME=$HOME/.cache
+export XDG_DATA_HOME=$HOME/.local/share
+
